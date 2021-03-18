9bbc91b2c1664a9ea8d6c5fa299824c352fd4d2f
Devon Walsh <devonwalsh@gmail.com>
2021-03-18 16:27:26 -0400

    Neatened up index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-document.getElementById("create-task-form").addEventListener("submit", function(event) {
+function addNewItem () {
   event.preventDefault();
   const ul = document.getElementById("list");
   const li = document.createElement("li");
   let newTask = document.getElementById("new-task-description").value;
   li.appendChild(document.createTextNode(newTask));
   ul.appendChild(li);
-})
+}
+
+document.getElementById("create-task-form").addEventListener("submit", addNewItem)
