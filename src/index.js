dbdc2c71976c0be8f306a1be8bf7fc9382841630
Devon Walsh <devonwalsh@gmail.com>
2021-03-18 16:24:52 -0400

    Added task submit functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.getElementById("create-task-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const ul = document.getElementById("list");
+  const li = document.createElement("li");
+  let newTask = document.getElementById("new-task-description").value;
+  li.appendChild(document.createTextNode(newTask));
+  ul.appendChild(li);
+})
