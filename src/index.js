3085eaac1757995e31e9f16d3ca43e0cccf0f966
Devon Walsh <devonwalsh@gmail.com>
2021-03-19 16:30:10 -0400

    Added functionality to delete items

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ function addNewItem () {
   let newTask = document.getElementById("new-task-description").value;
   li.id = Date.now();
   deleteButton.innerHTML = "X";
+  deleteButton.onclick = deleteItem;
   li.appendChild(document.createTextNode(newTask));
   li.appendChild(deleteButton);
   ul.appendChild(li);
 }
 
 function deleteItem () {
-  
+  let itemId = this.parentNode.id;
+  document.getElementById(itemId).remove();
 }
 
-document.getElementById("create-task-form").addEventListener("submit", addNewItem)
+document.getElementById("create-task-form").addEventListener("submit", addNewItem);
