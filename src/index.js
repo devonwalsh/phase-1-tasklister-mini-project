20c0feaeb60e88646bf775cd9d5bdb2ccc6f3558
Devon Walsh <devonwalsh@gmail.com>
2021-03-19 15:43:31 -0400

    Added button to list items

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ function addNewItem () {
   event.preventDefault();
   const ul = document.getElementById("list");
   const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
   let newTask = document.getElementById("new-task-description").value;
+  li.id = Date.now();
+  deleteButton.innerHTML = "X";
   li.appendChild(document.createTextNode(newTask));
+  li.appendChild(deleteButton);
   ul.appendChild(li);
+}
+
+function deleteItem () {
+  
 }
 
 document.getElementById("create-task-form").addEventListener("submit", addNewItem)
