d7f23cb63fc7c97fa25a6f220ef058ff19b98e14
Devon Walsh <devonwalsh@gmail.com>
2021-03-19 16:59:38 -0400

    Added user input to form; made task text editable

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ function addNewItem () {
   event.preventDefault();
   const ul = document.getElementById("list");
   const li = document.createElement("li");
+  const span = document.createElement("span");
   const deleteButton = document.createElement("button");
+
   let newTask = document.getElementById("new-task-description").value;
+  let newUser = document.getElementById("new-task-user").value;
+
   li.id = Date.now();
+  span.contentEditable = true;
   deleteButton.innerHTML = "X";
   deleteButton.onclick = deleteItem;
-  li.appendChild(document.createTextNode(newTask));
+  
+  li.appendChild(span);
   li.appendChild(deleteButton);
+  span.appendChild(document.createTextNode(`${newTask} \(${newUser}\)`));
   ul.appendChild(li);
 }
 
